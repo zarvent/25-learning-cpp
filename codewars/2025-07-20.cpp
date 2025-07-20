7f46d48f7d4b95c3815ff7ea6a5fbc0d7e21225f
zarvent <zarventt@gmail.com>
2025-07-20 00:52:09 -0400

    Implement is_digit function prototype and add validation examples

---
+++ codewars/2025-07-20.cpp
@@ -0,0 +1,23 @@
+/*
+Dada una cadena s, escriba un método (función) que devuelva true si es un número entero o flotante válido o false si no lo es. Ejemplos válidos,
+
+should return true:
+isDigit("3")
+isDigit("  3  ")
+isDigit("-3.23")
+
+
+should return false:
+isDigit("3-4")
+isDigit("  3   5")
+isDigit("3 5")
+isDigit("zero")
+https://www.codewars.com/kata/57126304cdbf63c6770012bd/train/cpp
+
+ */
+
+ #include <charconv>
+using namespace std;
+ bool is_digit(std::string s) {
+
+}
